0878e7b6d7b08c2501afda35571823f1f1aa234a
klay <15990184811@163.com>
2024-06-10 20:34:30 +0800

    Update file path pattern for mdx files in Blog config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -92,7 +92,7 @@ function createSearchIndex(allBlogs) {
 
 export const Blog = defineDocumentType(() => ({
   name: 'Blog',
-  filePathPattern: 'blog/**/*.mdx',
+  filePathPattern: 'blog/**/*.{mdx,md}',
   contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
